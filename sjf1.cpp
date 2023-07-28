38555dae0caf8c46ca293b2e0d82662fbde4964d
codewithRiC <138098044+codewithRiC@users.noreply.github.com>
2023-07-28 12:07:36 +0530

    Shortest job frst

---
+++ sjf1.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+int ct=0;
+struct Process {
+   int pid; 
+   int bt; 
+   int art; 
+};
+void findTurnAroundTime(Process proc[], int n, int wt[], int tat[]) {
+   for (int i = 0; i < n; i++){
+   tat[i] = proc[i].bt + wt[i];
+   ct+=proc[i].bt;
+   }
+}
+
+void findWaitingTime(Process proc[], int n, int wt[]) {
+   int rt[n];
+   for (int i = 0; i < n; i++)
+   rt[i] = proc[i].bt;
+   int complete = 0, t = 0, minm = INT_MAX;
+   int shortest = 0, finish_time;
+   bool check = false;
+   while (complete != n) {
+      for (int j = 0; j < n; j++) {
+         if ((proc[j].art <= t) && (rt[j] < minm) && rt[j] > 0) {
+            minm = rt[j];
+            shortest = j;
+            check = true;
+         }
+      }
+      if (check == false) {
+         t++;
+         continue;
+      }
+      
+      rt[shortest]--;
+      minm = rt[shortest];
+      if (minm == 0)
+         minm = INT_MAX;
+        
+         if (rt[shortest] == 0) {
+            complete++;
+            check = false;
+            finish_time = t + 1;
+           
+            wt[shortest] = finish_time -
+            proc[shortest].bt -
+            proc[shortest].art;
+            if (wt[shortest] < 0)
+               wt[shortest] = 0;
+         }
+         
+         t++;
+   }
+}
+
+void findavgTime(Process proc[], int n) {
+   int wt[n], tat[n], total_wt = 0,
+   total_tat = 0;
+  
+   findWaitingTime(proc, n, wt);
+  
+   findTurnAroundTime(proc, n, wt, tat);
+   cout << "Pr " <<"\t"<<  " AT " <<"\t"<<" BT " <<"\t"<< " WT " <<"\t"<< " TAT\n";
+   for (int i = 0; i < n; i++) {
+      total_wt = total_wt + wt[i];
+      total_tat = total_tat + tat[i];
+      cout << " " << proc[i].pid << "\t" << proc[i].art << "\t" << proc[i].bt << "\t " << wt[i] << "\t " << tat[i] << endl;
+   }
+   cout << "\nAverage waiting time = " << (float)total_wt / (float)n; 
+   cout << "\nAverage turn around time = " << (float)total_tat / (float)n;
+   cout<<"\nThrouput ="<<(float)n/ct;
+}
+
+int main() {
+
+   Process proc[10];
+   int n;
+    cout<<"Enter the no.of processes";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter the process";
+        cin>>proc[i].pid;
+        cout<<"Enter the Arrival time";
+        cin>>proc[i].art;
+        cout<<"Enter the burst time";
+        cin>>proc[i].bt;
+    }
+   findavgTime(proc, n);
+   return 0;
+}
